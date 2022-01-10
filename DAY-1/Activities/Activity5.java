cc341c7aecfce342523e13da476c93d0f2f1f513
shravanichandaka11 <shravanichandaka11@gmail.com>
2022-01-10 23:41:09 +0530

    new file updated

--- DAY-1/Activities/Activity5.java
+++ DAY-1/Activities/Activity5.java
@@ -1,5 +1,122 @@
 package com.basics;
 
 public class Activity5 {
-
+	public static void main(String[] args) {
+		
+		digitToWord(3,4,5);
+		
+	}
+	public static void digitToWord(int x, int y, int z)
+	{
+		
+			switch(x)
+			{
+			case 0:
+				System.out.println("zero");
+				break;
+			case 1:
+				System.out.println("one");
+				break;
+			case 2:
+				System.out.println("two");
+				break;
+			case 3:
+				System.out.println("three");
+				break;
+			case 4:
+				System.out.println("four");
+				break;
+			case 5:
+				System.out.println("five");
+				break;
+			case 6:
+				System.out.println("six");
+				break;
+			case 7:
+				System.out.println("seven");
+				break;
+			case 8:
+				System.out.println("eight");
+				break;
+			case 9:
+				System.out.println("nine");
+				break;
+				
+						
+			}
+			switch(y)
+			{
+			case 0:
+				System.out.println("zero");
+				break;
+			case 1:
+				System.out.println("one");
+				break;
+			case 2:
+				System.out.println("two");
+				break;
+			case 3:
+				System.out.println("three");
+				break;
+			case 4:
+				System.out.println("four");
+				break;
+			case 5:
+				System.out.println("five");
+				break;
+			case 6:
+				System.out.println("six");
+				break;
+			case 7:
+				System.out.println("seven");
+				break;
+			case 8:
+				System.out.println("eight");
+				break;
+			case 9:
+				System.out.println("nine");
+				break;
+				
+				
+				
+			}
+			switch(z)
+			{
+			case 0:
+				System.out.println("zero");
+				break;
+			case 1:
+				System.out.println("one");
+				break;
+			case 2:
+				System.out.println("two");
+				break;
+			case 3:
+				System.out.println("three");
+				break;
+			case 4:
+				System.out.println("four");
+				break;
+			case 5:
+				System.out.println("five");
+				break;
+			case 6:
+				System.out.println("six");
+				break;
+			case 7:
+				System.out.println("seven");
+				break;
+			case 8:
+				System.out.println("eight");
+				break;
+			case 9:
+				System.out.println("nine");
+				break;
+				
+				
+				
+			}
+					
+	}
 }
+
